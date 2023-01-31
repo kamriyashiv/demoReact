78a73ca5f8c5e4a1565ebac774210c7b1888bf8c
Thansingh <kamriyashiv@gmail.com>
2023-01-31 22:10:10 +0530

    update--

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import { Route, Routes } from 'react-router-dom';
+import Error from './components/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Page1/>}/>
+        <Route path='Page2' element={<Page2/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const Error =()=>{
+    return(
+        <>
+            <h1>Page Not Found</h1>
+        </>
+    );
+}
+
+export default Error;
